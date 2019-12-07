efcf11eab0416a75a1dbbfc0eec854fae3f1e059
MorozovSAIT4F19 <55760797+MorozovSAIT4F19@users.noreply.github.com>
2019-12-07 22:19:58 +0300

    Update ДЗ_1 (2 номер).sql

--- on_review/ДЗ_1 (2 номер).sql
+++ on_review/ДЗ_1 (2 номер).sql
@@ -1,3 +1,4 @@
+-- В таблицу выносятся rat_id и rating_record_id, но rat_id не имеет смысла, а rating_record_id так и не был прокомментирован
 DROP TABLE if exists rat_info
 ;
 CREATE TABLE rat_info AS
@@ -9,5 +10,6 @@ DROP TABLE if exists info_comp
 ;
 CREATE TABLE info_comp AS
 SELECT ent_name, okpo, ogrn, inn,  finst
+-- PR: отношение "ratings_task" не существует (видимо, имелось в виду actions)
 FROM ratings_task GROUP BY ent_name, okpo, ogrn, inn,  finst;
 ALTER TABLE info_comp ADD PRIMARY KEY (ent_name)
